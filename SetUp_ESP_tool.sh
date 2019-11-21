a08be0a26f8793d2a12626d87aeb6e081fab4259
jibi8086 <36498166+jibi8086@users.noreply.github.com>
2019-11-21 12:28:39 +0530

    Update SetUp_ESP_tool.sh

--- SetUp_ESP_tool.sh
+++ SetUp_ESP_tool.sh
@@ -1,1 +1,3 @@
-sudo apt-get install git wget flex bison gperf python python-pip python-setuptools python-serial python-click python-cryptography python-future python-pyparsing python-pyelftools cmake ninja-build ccache
+#URL:https://docs.espressif.com/projects/esp-idf/en/latest/get-started/index.html#get-started-get-esp-idf
+
+#sudo apt-get install git wget flex bison gperf python python-pip python-setuptools python-serial python-click python-cryptography python-future python-pyparsing python-pyelftools cmake ninja-build ccache
