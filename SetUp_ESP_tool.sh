98408964c66bd09e27fb02d2447905c5731233fb
jibi8086 <36498166+jibi8086@users.noreply.github.com>
2019-11-21 12:26:33 +0530

    Create SetUp_ESP_tool.sh

---
+++ SetUp_ESP_tool.sh
@@ -0,0 +1,1 @@
+sudo apt-get install git wget flex bison gperf python python-pip python-setuptools python-serial python-click python-cryptography python-future python-pyparsing python-pyelftools cmake ninja-build ccache
