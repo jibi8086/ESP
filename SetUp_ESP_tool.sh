bf2a9f71c08fcbcd11ec9d42dc469875aa66158b
jibi8086 <36498166+jibi8086@users.noreply.github.com>
2019-11-29 13:11:53 +0530

    Update SetUp_ESP_tool.sh

--- SetUp_ESP_tool.sh
+++ SetUp_ESP_tool.sh
@@ -1,3 +1,9 @@
 #URL:https://docs.espressif.com/projects/esp-idf/en/latest/get-started/index.html#get-started-get-esp-idf
 
 #sudo apt-get install git wget flex bison gperf python python-pip python-setuptools python-serial python-click python-cryptography python-future python-pyparsing python-pyelftools cmake ninja-build ccache
+
+
+#Arduino firebase esp setup
+
+
+https://arduino-esp8266.readthedocs.io/en/latest/faq/a04-board-generic-is-unknown.html
